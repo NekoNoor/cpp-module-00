10f982449cf3cc7aa0ed00c5ee76878483767f82
Norah Hannah Schat <nschat@student.codam.nl>
2021-01-25 13:17:51 +0100

    feat: finish myawesomephonebook fix gitignores

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nschat <nschat@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/18 12:17:57 by nschat        #+#    #+#                 */
-/*   Updated: 2021/01/25 12:16:21 by nschat        ########   odam.nl         */
+/*   Updated: 2021/01/25 13:15:40 by nschat        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,74 @@
 #include <iomanip>
 #include "Contact.hpp"
 
-int	main(void)
+static std::string	truncate(std::string str)
+{
+	if (str.length() > 10)
+		return (str.substr(0, 9) + ".");
+	return (str);
+}
+
+static Contact		add_contact()
+{
+	Contact contact;
+	std::string fn, ln, nn, l, pa, ea, pn, bd, fm, uc, ds;
+
+	std::cout << std::setw(17) << "first name: ";
+	std::getline(std::cin, fn);
+	std::cout << std::setw(17) << "last name: ";
+	std::getline(std::cin, ln);
+	std::cout << std::setw(17) << "nickname: ";
+	std::getline(std::cin, nn);
+	std::cout << std::setw(17) << "login: ";
+	std::getline(std::cin, l);
+	std::cout << std::setw(17) << "postal address: ";
+	std::getline(std::cin, pa);
+	std::cout << std::setw(17) << "email address: ";
+	std::getline(std::cin, ea);
+	std::cout << std::setw(17) << "phone number: ";
+	std::getline(std::cin, pn);
+	std::cout << std::setw(17) << "birthday date: ";
+	std::getline(std::cin, bd);
+	std::cout << std::setw(17) << "favorite meal: ";
+	std::getline(std::cin, fm);
+	std::cout << std::setw(17) << "underwear color: ";
+	std::getline(std::cin, uc);
+	std::cout << std::setw(17) << "darkest secret: ";
+	std::getline(std::cin, ds);
+	contact.set_all(fn, ln, nn, l, pa, ea, pn, bd, fm, uc, ds);
+	return (contact);
+}
+
+static void			print_preview(Contact phonebook[8], int counter)
+{
+	for (int i = 0; i < counter; i++)
+	{
+		std::cout << std::setw(10) << i << '|';
+		std::cout << std::setw(10) << truncate(phonebook[i].get_firstname()) << '|';
+		std::cout << std::setw(10) << truncate(phonebook[i].get_lastname()) << '|';
+		std::cout << std::setw(10) << truncate(phonebook[i].get_nickname()) << std::endl;
+	}
+}
+
+static void			print_contact(Contact contact)
+{
+	std::cout << std::setw(17) << "first name: " << contact.get_firstname() << std::endl;
+	std::cout << std::setw(17) << "last name: " << contact.get_lastname() << std::endl;
+	std::cout << std::setw(17) << "nickname: " << contact.get_nickname() << std::endl;
+	std::cout << std::setw(17) << "login: " << contact.get_login() << std::endl;
+	std::cout << std::setw(17) << "postal address: " << contact.get_postaladdress() << std::endl;
+	std::cout << std::setw(17) << "email address: " << contact.get_emailaddress() << std::endl;
+	std::cout << std::setw(17) << "phone number: " << contact.get_phonenumber() << std::endl;
+	std::cout << std::setw(17) << "birthday date: " << contact.get_birthdaydate() << std::endl;
+	std::cout << std::setw(17) << "favorite meal: " << contact.get_favoritemeal() << std::endl;
+	std::cout << std::setw(17) << "underwear color: " << contact.get_underwearcolor() << std::endl;
+	std::cout << std::setw(17) << "darkest secret: " << contact.get_darkestsecret() << std::endl;
+}
+
+int					main(void)
 {
 	Contact phonebook[8];
-	int		i = 0;
+	int		counter = 0;
 
 	for (std::string line; std::getline(std::cin, line);)
 	{
@@ -25,53 +89,17 @@ int	main(void)
 			break;
 		else if (line.compare("ADD") == 0)
 		{
-			std::string fn, ln, nn, l, pa, ea, pn, bd, fm, uc, ds;
-
-			std::cout << "Enter first name:" << std::endl;
-			std::getline(std::cin, fn);
-			std::cout << "Enter last name:" << std::endl;
-			std::getline(std::cin, ln);
-			std::cout << "Enter nickname:" << std::endl;
-			std::getline(std::cin, nn);
-			std::cout << "Enter login:" << std::endl;
-			std::getline(std::cin, l);
-			std::cout << "Enter postal address:" << std::endl;
-			std::getline(std::cin, pa);
-			std::cout << "Enter email address:" << std::endl;
-			std::getline(std::cin, ea);
-			std::cout << "Enter phone number:" << std::endl;
-			std::getline(std::cin, pn);
-			std::cout << "Enter birthday date:" << std::endl;
-			std::getline(std::cin, bd);
-			std::cout << "Enter favorite meal:" << std::endl;
-			std::getline(std::cin, fm);
-			std::cout << "Enter underwear color:" << std::endl;
-			std::getline(std::cin, uc);
-			std::cout << "Enter darkest secret:" << std::endl;
-			std::getline(std::cin, ds);
-			phonebook[i].set_all(fn, ln, nn, l, pa, ea, pn, bd, fm, uc, ds);
-			if (i < 8)
-				i++;
-			else
-				i = 0;
+			phonebook[counter] = add_contact();
+			if (counter < 8)
+				counter++;
 		}
 		else if (line.compare("SEARCH") == 0)
 		{
-			for (int j = 0; j < 8; j++)
-			{
-				std::cout << j << std::endl;
-				std::cout << phonebook[j].get_firstname() << std::endl;
-				std::cout << phonebook[j].get_lastname() << std::endl;
-				std::cout << phonebook[j].get_nickname() << std::endl;
-				std::cout << phonebook[j].get_login() << std::endl;
-				std::cout << phonebook[j].get_postaladdress() << std::endl;
-				std::cout << phonebook[j].get_emailaddress() << std::endl;
-				std::cout << phonebook[j].get_phonenumber() << std::endl;
-				std::cout << phonebook[j].get_birthdaydate() << std::endl;
-				std::cout << phonebook[j].get_favoritemeal() << std::endl;
-				std::cout << phonebook[j].get_underwearcolor() << std::endl;
-				std::cout << phonebook[j].get_darkestsecret() << std::endl;
-			}
+			print_preview(phonebook, counter);
+			std::cout << "index: ";
+			int index;
+			std::cin >> index;
+			print_contact(phonebook[index]);
 		}
 		continue;
 	}
